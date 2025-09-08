9bfa56b3ca35bcc70d72db08d5d089c93d563bb3
PeSzpak <dropszpalves@gmail.com>
2025-09-08 11:34:10 -0300

    feat: Add validation with class-validator for users DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
-  id: number;
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsBoolean()
+  amin: boolean;
 }
