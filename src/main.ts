88a018180fd4e5c8c00064ff7a81ef855a9d61c5
PeSzpak <dropszpalves@gmail.com>
2025-09-12 13:15:12 -0300

    feat: add database error interceptor handle for unique contraint (email)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConflictInterceptor } from './common/filters/http-exception/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/filters/http-exception/errors/interceptors/database.interceptor';
+import { NotFoundInterceptor } from './common/filters/http-exception/errors/interceptors/notfound.interceptor';
 import { UnauthorizedInterceptor } from './common/filters/http-exception/errors/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,8 +16,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+
+  app.useGlobalInterceptors(
+    // app.useGlobalFilters(new HttpExceptionFilter());
+    new ConflictInterceptor(),
+    new DatabaseInterceptor(),
+    new UnauthorizedInterceptor(),
+    new NotFoundInterceptor(),
+  );
+
   await app.listen(process.env.PORT || 3000);
 }
 void bootstrap();
