cf8d928b4bdb3ac187d0a2baf882695ac18b0980
PeSzpak <dropszpalves@gmail.com>
2025-09-22 14:49:33 -0300

    feat: implemented the authon email parameter for create posts with error handler validation

--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -1,4 +1,6 @@
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 import { PostEntity } from '../entities/post.entity';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { Injectable } from '@nestjs/common';
@@ -9,8 +11,32 @@ export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    const { authorEmail } = createPostDto;
+
+    delete createPostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError('Author not found.');
+    }
+
+    const data: Prisma.PostCreateInput = {
+      ...createPostDto,
+
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
     return await this.prisma.post.create({
-      data: createPostDto,
+      data,
     });
   }
 
