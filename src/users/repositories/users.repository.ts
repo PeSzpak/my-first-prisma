d2611fb119e3684c1e775f16050ae48410c4ccb4
PeSzpak <dropszpalves@gmail.com>
2025-09-22 15:57:59 -0300

    refactor: altere users.repository for correct error handler

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -3,32 +3,23 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { UserEntity } from '../entities/user.entity';
-import { isPrismaError } from 'src/common/filters/http-exception/errors/utils/is-prisma-error.util';
-import { handleDatabaseErrors } from 'src/common/filters/http-exception/errors/utils/handle-database-errors.utils';
 
 @Injectable()
 export class UsersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
-    try {
-      return await this.prisma.user.create({
-        data: createUserDto,
-        include: {
-          posts: {
-            select: {
-              title: true,
-              createdAt: true,
-            },
+    return this.prisma.user.create({
+      data: createUserDto,
+      include: {
+        posts: {
+          select: {
+            title: true,
+            createdAt: true,
           },
         },
-      });
-    } catch (error) {
-      if (isPrismaError(error)) {
-        throw handleDatabaseErrors(error);
-      }
-      throw error;
-    }
+      },
+    });
   }
 
   async findAll(): Promise<UserEntity[]> {
@@ -46,7 +37,9 @@ export class UsersRepository {
 
   async findOne(id: number): Promise<UserEntity | null> {
     return this.prisma.user.findUnique({
-      where: { id },
+      where: {
+        id,
+      },
       include: {
         posts: {
           select: {
@@ -59,30 +52,27 @@ export class UsersRepository {
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
-    try {
-      return await this.prisma.user.update({
-        where: { id },
-        data: updateUserDto,
-        include: {
-          posts: {
-            select: {
-              title: true,
-              createdAt: true,
-            },
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+      include: {
+        posts: {
+          select: {
+            title: true,
+            createdAt: true,
           },
         },
-      });
-    } catch (error) {
-      if (isPrismaError(error)) {
-        throw handleDatabaseErrors(error);
-      }
-      throw error;
-    }
+      },
+    });
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<UserEntity> {
     return this.prisma.user.delete({
-      where: { id },
+      where: {
+        id,
+      },
     });
   }
 }
