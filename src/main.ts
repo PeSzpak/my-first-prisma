d0f3d35389dfa8f2db7af76db47fde798d105a99
PeSzpak <dropszpalves@gmail.com>
2025-09-23 15:37:52 -0300

    improv: add swagger for api docs with decorators on the controllers

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,19 @@ import { ConflictInterceptor } from './common/filters/http-exception/errors/inte
 import { DatabaseInterceptor } from './common/filters/http-exception/errors/interceptors/database.interceptor';
 import { NotFoundInterceptor } from './common/filters/http-exception/errors/interceptors/notfound.interceptor';
 import { UnauthorizedInterceptor } from './common/filters/http-exception/errors/interceptors/unauthorized.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The Simple Blog API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'titulo do post' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ description: 'conteudo do post' })
   @IsString()
   @IsOptional()
   content: string | null;
 
+  @ApiProperty({ description: 'Email do criador do post' })
   @IsEmail()
   authorEmail?: string;
 }
